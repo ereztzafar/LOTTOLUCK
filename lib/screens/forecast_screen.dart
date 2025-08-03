c42ffae8b37967043d962a7703dde899f4c88371
ereztzafar <erez.zafar@gmail.com>
2025-08-03 12:45:05 +0300

    Create forecast_screen.dart

---
+++ lib/screens/forecast_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ForecastScreen extends StatelessWidget {
+  const ForecastScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('תחזית אסטרולוגית'),
+      ),
+      body: const Center(
+        child: Text(
+          'כאן תופיע התחזית האסטרולוגית של היום',
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
