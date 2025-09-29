332250d4e4dd06fb8fbc41604b33f869029f47df
Erez Zafar <erez.zafar@gmail.com>
2025-09-29 22:23:20 +0300

    Add build/web for static deploy

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -41,7 +41,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "cc4fea234c1fdaf73cf77147678020fd
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "67cd10bc7b39ee5ac5cd88db230299c6",
+"flutter_bootstrap.js": "14d7557420bb10def6148b6287ffad37",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
